305b3839bf1a8bf1869b4344a2b0d96f2b1a662d
jFriedli <jonas.friedli@hotmail.com>
2019-03-07 18:36:12 +0100

    make free passes required

--- src/ch/jfriedli/springdemo/mvc/Customer.java
+++ src/ch/jfriedli/springdemo/mvc/Customer.java
@@ -14,9 +14,11 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	//Not null does not work in primitive types (eg. int)
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to ten")
-	private int freePasses;
+	private Integer freePasses;
 	
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="enter a valid postal code")
@@ -38,11 +40,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
