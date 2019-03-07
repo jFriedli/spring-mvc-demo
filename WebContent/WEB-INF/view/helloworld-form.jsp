bd26e04112315ac89176c82b84da473d0204aaeb
jFriedli <jonas.friedli@hotmail.com>
2019-03-07 15:59:08 +0100

    add request parameter

--- WebContent/WEB-INF/view/helloworld-form.jsp
+++ WebContent/WEB-INF/view/helloworld-form.jsp
@@ -3,7 +3,7 @@
 	<title>Hello World - Input Form </title>
 <body>
 
-	<form action="processFormVersionTwo" method="GET">
+	<form action="processFormVersionThree" method="GET">
 		
 		<input type="text" name="studentName" placeholder="What's your name" />
 		
--- src/ch/jfriedli/springdemo/mvc/HelloWorldController.java
+++ src/ch/jfriedli/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -23,6 +24,18 @@ public class HelloWorldController {
 	public String letsShoutDude(HttpServletRequest request, Model model) {
 		
 		String name = request.getParameter("studentName");
+		
+		name = name.toUpperCase();
+		
+		String result = "Yo! " + name;
+		
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String name, HttpServletRequest request, Model model) {
 		
 		name = name.toUpperCase();
 		
